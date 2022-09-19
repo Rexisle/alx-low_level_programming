9482e837d6a3b030cabf16bf5902f85fced38684
Promise Okere <promiseokere0@gmail.com>
2022-09-19 12:38:35 -0700

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,13 +7,7 @@
 * @n: input
 * Return: n
 */
-int main (void)
+void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+	*n = 98;
 }
